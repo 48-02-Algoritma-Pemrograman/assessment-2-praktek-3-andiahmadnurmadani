254952ac7451a68b67ea10a5d162fb3f09e2bf16
andiahmadnurmadani <106458721+andiahmadnurmadani@users.noreply.github.com>
2024-12-12 13:15:31 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 public class Nomor1 {
+  static Scanner input = new Scanner(System.in);
+
+  static double luas(int alas, int tinggi){
+    double hasil = (alas * tinggi) / 2;
+    return hasil;
+  }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-
+        int tinggi = input.nextInt();
+        System.out.println(luas(N, tinggi));
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
